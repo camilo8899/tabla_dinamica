6292020e62c4f0daf1b0849846a8b9b39d934233
camilo8899 <43257373+camilo8899@users.noreply.github.com>
2018-10-22 08:49:46 -0500

    Update Update.php

--- php/Update.php
+++ php/Update.php
@@ -5,7 +5,7 @@ $codigo    = $_REQUEST['codigou'];
 $nombre    = $_POST['nombreu'];
 $telefono  = $_POST['telefonou'];
 $direccion = $_POST['direccionu'];
-mysql_query("UPDATE alumnos SET nombres='$nombre', telefono='$telefono', direccion='$direccion' WHERE codigo='$codigo'", $link);
+mysqli_query("UPDATE alumnos SET nombres='$nombre', telefono='$telefono', direccion='$direccion' WHERE codigo='$codigo'", $link);
 echo 'Datos Actualizados';
 header("Location:index.php");
 ?>
