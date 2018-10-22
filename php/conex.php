79ae540a370e148c3709ccca2487b4046509a407
camilo8899 <43257373+camilo8899@users.noreply.github.com>
2018-10-22 09:10:50 -0500

    Update conex.php

--- php/conex.php
+++ php/conex.php
@@ -1,16 +1,17 @@
 <?php
 function conectarse()
 {
-    if (!($link = mysqli_connect("localhost", "root", "root"))) {
-        echo "Error conectandose a la base de datos";
-        exit();
-    }
-    if (!mysqli_select_db("lista_alum", $link)) {
-        echo "Error seleccionando la base de datos.";
-        exit();
+    //if (!($link = mysqli_connect("localhost", "root", "root"))) {
+        //echo "Error conectandose a la base de datos";
+        //exit();
+    //}
+    //if (!mysqli_select_db("lista_alum", $link)) {
+        //echo "Error seleccionando la base de datos.";
+        //exit();
 
-    }
-    return $link;
+    //}
+    //return $link;
+    
 
 }
 ?>
