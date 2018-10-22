14f7d690ed9557382c3f2e69e1f71d5e804a63d4
camilo8899 <43257373+camilo8899@users.noreply.github.com>
2018-10-22 08:44:28 -0500

    Update delete.php

--- php/delete.php
+++ php/delete.php
@@ -2,7 +2,7 @@
 include "conex.php";
 $link   = conectarse();
 $codigo = $_REQUEST['id'];
-mysql_query("delete from alumnos where codigo='$codigo'", $link);
+mysqli_query("delete from alumnos where codigo='$codigo'", $link);
 echo 'Datos eleminados';
 header("Location:index.php");
 ?>
